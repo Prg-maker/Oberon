a012fa2c0794879cc97347aef56b3910c4303a92
Prg-maker <df3296709@gmail.com>
2022-04-15 11:27:26 -0300

    testing end UserRepository

--- backend/src/repositorys/UserRepository.js
+++ backend/src/repositorys/UserRepository.js
@@ -1,7 +1,6 @@
 const crypto = require('crypto')
 
 const {promisify} = require('util')
-const path = require('path')
 
 const { readFile, writeFile} = require('fs')
 
@@ -28,7 +27,7 @@ class UserRepository {
 
     return true
   }
-  async createUser(user){
+  async createUser(user ){
     const dados = await  this.getFileAndRead()
 
 
@@ -37,28 +36,67 @@ class UserRepository {
       if(!user){
         throw  Error('user not passed, try again')
       }
+      const _id = this._id
+
       const userId = {
-        _id: this._id,
+        _id,
         ...user
       }
       const dadosEnd = [
         ...dados,
         userId
       ]
-
-      const response = await this.writeFile(dadosEnd)
-
-      return response
-      
-
+      await this.writeFile(dadosEnd)
+      return dadosEnd
     }catch(err){
       throw new  Error('user not create' )
     }
+  }
+  async listAllUsers(){
+    try{
 
+      const file = await this.getFileAndRead()
+      if(file == []){
+        return 'not have   users'
+      }
+
+      return file
+    }catch(err){
+      throw Error('no is possible to list user')
+    }
   }
 
+  async listOneUser(_id){
+ 
+    try{
+      const file = await this.getFileAndRead()
+      const user = file.filter(user =>  user._id === _id)
+      return user
 
+    }catch(err){
+      throw Error('no is possible to list user')
+    }
+  }
 
+  async deleteOne(_id){
+    try{
+      const file = await this.getFileAndRead()
+
+      const index = file.findIndex(item => item._id == _id)
+
+      if(index == -1){
+        throw Error('user not exist')
+      }
+      file.splice(index , 1)
+  
+  
+      await this.writeFile([...file])
+      return true
+
+    }catch(err){
+      throw Error('user not delete')
+    }
+  }
 
 
 
--- backend/src/repositorys/_test/_user.test.js
+++ backend/src/repositorys/_test/_user.test.js
@@ -1,19 +1,42 @@
 const User = require('../UserRepository')
 
+
+
+
 const usuario_cadastrar = {
-  name: 'Daniel',
+  name: 'DanielFernandesSilva',
   password: 'daniell123',
   nameGithub: 'Prg-Maker'
 }
 
-
 describe('testing User' , ()=> {
   it('should creating a user' , async ()=>{
 
-    const user = await User.createUser(usuario_cadastrar)
+    const [user] = await User.createUser(usuario_cadastrar)
+
+    const [expectUser] = await User.listOneUser(user._id)
+    expect(user).toEqual(expectUser)
+
+  })
+
+
+  it('should list as users', async()=> {
+    const users= await User.listAllUsers()
+
+    expect(users).toBeTruthy()
+
+  })
+
+  it('should list a user for id' , async ()=> {
+    const user = await User.listOneUser('2cdc83c4-2418-4ae0-b504-24448fdf837a')
 
     expect(user).toBeTruthy()
 
+  })
 
+  it('should delete a user for id' , async ()=> {
+    const user = await User.deleteOne('70269f17-a187-4040-a679-61d2e7b5e16a')
+
+    expect(user).toBeTruthy()
   })
 })
