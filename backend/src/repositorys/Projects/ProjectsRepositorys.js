f83506bcad78199d2963f3de8211313f0acae57c
Prg-maker <df3296709@gmail.com>
2022-05-05 21:02:49 -0300

    testing to create project with prisma

--- backend/src/repositorys/Projects/ProjectsRepositorys.js
+++ backend/src/repositorys/Projects/ProjectsRepositorys.js
@@ -9,6 +9,45 @@ class ProjectsRepositorys {
 
   async createProject(project){
 
+
+    if(!project.userId){
+      throw new Error('user not exist')
+    }
+
+    try{
+
+      const {
+        userId,
+        title,
+        details,
+        repositoryGit,
+        progress,
+        createdAt,
+        updatedAt
+      } = project
+  
+      await prismaClient.projects.create({
+        data:{
+          userId,
+          title,
+          details,
+          repositoryGit,
+          progress,
+          createdAt,
+          updatedAt
+        }
+      })
+
+      return true
+
+    }catch(err){
+      return {
+        error:"error when creating a project"
+      }
+    }
+
+  
+
    
   }
 
