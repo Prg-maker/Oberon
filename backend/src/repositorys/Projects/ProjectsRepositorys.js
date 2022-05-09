37a44378ed57e912655dc7d09c1d7141b5fc2335
Prg-maker <df3296709@gmail.com>
2022-05-09 10:19:05 -0300

    writing test

--- backend/src/repositorys/Projects/ProjectsRepositorys.js
+++ backend/src/repositorys/Projects/ProjectsRepositorys.js
@@ -1,14 +1,9 @@
 const {v4} = require('uuid')
 const prismaClient = require('../../database/prisma')
 
-
 class ProjectsRepositorys {
 
-
- 
-
   async createProject(project){
-
 
     if(!project.userId){
       throw new Error('user not exist')
@@ -45,31 +40,68 @@ class ProjectsRepositorys {
         error:"error when creating a project"
       }
     }
-
-  
-
-   
   }
-
 
 
   async listAllProjectUser(userId){
+
+    if(!userId){
+      throw new Error('the userId was not passed')
+    }
+
+
+    try{
+      const projects = await prismaClient.projects.findMany({
+        where:{
+          userId
+        }
+      })
+      return {
+        data:projects
+      }
+    }catch(err){
+      return {
+        error: "user does not exist"
+      }
+    }
+
+
+
+  }
+
+
+  async listOneProject(id){
+    
+    if(!id){
+      throw new Error('id did not pass ')
+    }
+
+
+    try{
+      const projects = await prismaClient.projects.findFirst({
+        where:{
+          id
+        }
+      })
+
+      return projects
+
+    }catch(err){
+      return {
+        error:"project dost not exist"
+      }
+    }
+  }
+
+  async deleteOneProject(id){
+
+    if(!id){
+      return {
+        message: "id was not passed"
+      }
+    }
    
   }
-
-
-  async listOneProject(_id){
-    
-
-
-  
-    
-  }
-
-  async deleteOneProject(_id){
-   
-  }
-
  
 }
 
--- backend/src/repositorys/Projects/_test/_projects.test.js
+++ backend/src/repositorys/Projects/_test/_projects.test.js
@@ -36,36 +36,79 @@ describe('testing project' , ()=> {
   })
 
   it('should create one project' , async ()=> {
-    const sut = await ProjectsRepositorys.createProject(Project_Test_fake)
+    //const sut = await ProjectsRepositorys.createProject(Project_Test_fake)
 
-    expect(sut).toBeTruthy()
+    //expect(sut).toBeTruthy()
   })
 
-  it('should return one message if the user has no project, the user does not have any a project' , async ()=> {
-   
+  it('should verify if the user dost exist' , async ()=> {
+    const sut = ProjectsRepositorys.listAllProjectUser('')
+
+    expect(sut).rejects.toBeTruthy()
+
+  })
+
+  it('should verify if the user dost exist' , async ()=> {
+    return ProjectsRepositorys.listAllProjectUser({userId: ""}).catch(sut => {
+      expect(sut).rejects.toThrow()
+    })
 
   })
 
   it('Should must list all projects, by userId' , async ()=> {
-   
+    const userId = '015c5cbe-ba3f-4839-98a6-5e18551e1c7d'
+    const sut = await ProjectsRepositorys.listAllProjectUser(userId)
+
+
+    expect(sut.data[0].userId).toBe(userId)
   })
 
+
+
+
   it('should validate if the id is passed' , async () => {
-  
+    return ProjectsRepositorys.listOneProject({id: ""}).catch(sut => {
+      expect(sut).toThrow()
+    })
+
   })
 
   it('Should validate if the project exist, if he does not exist, return one error ' ,async () => {
-   
+    
+    return ProjectsRepositorys.listOneProject({id: "1"}).catch(sut => {
+      expect(sut).rejects.toThrow()
+    })
 
   })
 
   it('Should get one project by id' , async ()=> {
- 
+
+    const userId = '015c5cbe-ba3f-4839-98a6-5e18551e1c7d'
+    const id = '9748d664-d4c0-4d2d-bd61-a1b366b6cc46'
+    
+    const sut = await ProjectsRepositorys.listOneProject(id)
+
+
+    expect(sut.id).toBe(id)
+    expect(sut.userId).toBe(userId)
+  })
+
+
+  /*delete*/
+
+  it('should check if the id was passed' ,async () => {
+       
+    return ProjectsRepositorys.deleteOneProject('').catch(sut => {
+      expect(sut).rejects.toThrowError()
+    })
 
   })
 
-  it('Should delete one project by id' , async ()=> {
+  it('should check if the project exists' ,async () => {
 
+  })
+  
+  it('Should delete one project by id' , async ()=> {
 
   })
 
