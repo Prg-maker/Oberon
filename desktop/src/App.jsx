2d9ad369edb78a56a98fbfea6358f7a1810c26f7
Prg-maker <df3296709@gmail.com>
2022-05-15 10:35:42 -0300

    add electron for desktop and style

---
+++ desktop/src/App.jsx
@@ -0,0 +1,12 @@
+import { Home } from "./pages/Home";
+import GlobalStyles from "./styles/GlobalStyles";
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <GlobalStyles/>
+    </div>
+  );
+}
+
+export default App;
---
+++ desktop/src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import {
+  Container,
+} from './styles'
+
+
+export function Home(){
+  return(
+    <Container>
+      <h1>Aqui</h1>
+    </Container>
+  )
+}
+
