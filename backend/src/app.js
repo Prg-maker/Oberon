bb69d25780a53f1e90396bd51653bbba477a2866
Prg-maker <df3296709@gmail.com>
2022-05-26 09:09:56 -0300

    add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,11 @@ require('dotenv/config')
 const express= require('express')
 const router = require('./routes')
 const app = express()
+const cors = require('cors')
 
+app.use(cors({
+  origin:"*"
+}))
 app.use(express.json())
 app.use(router)
 
