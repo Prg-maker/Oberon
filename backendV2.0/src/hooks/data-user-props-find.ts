a94eef7d7de672c8b101180dc97283166c4c1bab
Prg-maker <df3296709@gmail.com>
2022-07-17 11:58:34 -0300

    add hook findyByname

---
+++ backendV2.0/src/hooks/data-user-props-find.ts
@@ -0,0 +1,11 @@
+export interface DataProps{
+  name:string
+}
+
+export interface Error{
+  messageError: Error 
+}
+
+export interface UseFindUserDataProps{
+  findByName: (data:DataProps) => Promise<Error | null>
+}
---
+++ backendV2.0/src/hooks/useFindUser.ts
@@ -0,0 +1,10 @@
+import {UseFindUserDataProps , DataProps , Error} from './data-user-props-find'
+
+
+
+export class useFindUser implements UseFindUserDataProps{
+ async findByName (data:DataProps){
+  
+  return null
+ }
+}
