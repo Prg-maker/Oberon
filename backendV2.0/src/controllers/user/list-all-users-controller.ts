a971cdaaea28b9eae6cd70298d8ecabbf6f7065f
Prg-maker <df3296709@gmail.com>
2022-08-15 20:40:34 -0300

    add route list all

--- backendV2.0/src/controllers/user/list-all-users-controller.ts
+++ backendV2.0/src/controllers/user/list-all-users-controller.ts
@@ -1,11 +1,28 @@
-interface Request{
-  body:{
-    id:string
-  }
-}
+import {  Response , Request} from 'express'
+
+import {PrismaUserRepository} from '../../repository/prisma/prisma-user-repository'
+import { ListAllUsersUseCase} from '../../use-cases/user/list-all-users-use-case'
 
 export class ListAllUsersController{
-  async handle(request:Request ){
+  async handle(request:Request, response:Response){
+    const prismaUserRepository  = new PrismaUserRepository()
+    const listAllUsersUseCase = new ListAllUsersUseCase(prismaUserRepository)
 
+
+    try{
+
+      const projects = await listAllUsersUseCase.execute()
+
+      console.log(projects)
+      return response.status(200).json(projects)
+
+    }catch({message}){
+      return response.status(400).json({
+        err:message
+      })
+    }
+
+    
+    
   }
 }
--- backendV2.0/src/routes.ts
+++ backendV2.0/src/routes.ts
@@ -1,10 +1,12 @@
 import {CreateUserController} from './controllers/user/create-user-controller'
+import {ListAllUsersController} from './controllers/user/list-all-users-controller'
 
 import {Router} from 'express'
 
 const router = Router()
 
 router.post('/', new CreateUserController().handle)
+router.get('/', new ListAllUsersController().handle)
 
 export {
   router
