3868910688d4e27a9ccd37d6c95fb63ba2ad1fa5
Prg-maker <df3296709@gmail.com>
2022-08-23 11:05:21 -0300

    add cors

--- backendV2.0/src/server.ts
+++ backendV2.0/src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express'
 import { router } from './routes'
+import cors from 'cors' 
 
 const server = express()
 
+server.use(cors({
+  origin:"*"
+}))
 server.use(express.json())
 server.use(router)
 
