bf409e272f54334f398532459f8ecbbcf14f891d
Prg-maker <df3296709@gmail.com>
2022-09-01 10:23:00 -0300

    create rotuer login

--- desktop/src/pages/Login/index.jsx
+++ desktop/src/pages/Login/index.jsx
@@ -8,9 +8,31 @@ import {
 } from './styles'
 
 import {ArrowLeft, ArrowRight} from 'phosphor-react'
-    
+
+import {useState}from 'react'
+import {api} from '../../services'
     
 export function Login(){
+
+  const [password, setPassword] = useState('')
+  const [name , setName] = useState('')
+
+  async function handleSingIn(event){
+    event.preventDefault()
+    try{
+      const {data} = await api.post('/login ', {
+        name,
+        password
+      })
+      
+      localStorage.setItem('token' ,data.token )
+      console.log(data)
+    }catch({message}){
+      console.log('message')
+    }
+
+  
+  }
 
 
   return(
@@ -24,7 +46,7 @@ export function Login(){
         <div className='name'>
           <label htmlFor="nome">Nome</label>
           <Title>Nome</Title>
-          <Input  placeholder='Name' type="nome" />
+          <Input onChange={e => setName(e.target.value)} value={name} placeholder='Name' type="nome" />
         </div>
 
 
@@ -33,7 +55,7 @@ export function Login(){
         <div className='password'>
           <label htmlFor="senha">Senha</label>
             <Title>Senha</Title>
-            <Input placeholder='Senha' type="password" id='senha'/>
+            <Input onChange={e => setPassword(e.target.value)}  placeholder='Senha' type="password" id='senha'/>
         </div>
         
         <div className='buttons'>
@@ -44,7 +66,7 @@ export function Login(){
         </ButtonBack>
 
 
-        <ButtonEnterInProfile >
+        <ButtonEnterInProfile  onClick={handleSingIn}>
           Entrar
           <ArrowRight size={25} weight="bold" />
         </ButtonEnterInProfile>
